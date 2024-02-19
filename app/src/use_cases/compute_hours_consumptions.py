2e0b763cbb9b345103ace2b54025950958f7cc56
LukaBch <lucas.banchereau@gmail.com>
2024-02-19 20:13:59 +0100

    refacto in compute_hour_consumptions

--- app/src/use_cases/compute_hours_consumptions.py
+++ app/src/use_cases/compute_hours_consumptions.py
@@ -12,70 +12,36 @@ def compute_hour_consumptions(selected_appliances, max_energy_consumption):
     - h_L is an integer between 4 and 24
     """
 
-    coefficients = {}
-    for i in APPLIANCES.keys():
-        coefficients[i] = round(APPLIANCES[i]["power"]/1000, 2) if i in selected_appliances else 0
+    coefficients = {i: round(APPLIANCES[i]["power"] / 1000, 2) if i in selected_appliances else 0 for i in APPLIANCES.keys()}
 
     lp_problem = LpProblem(name="Maximize_Objective", sense=LpMaximize)
 
-    # Define variables
-    x_1 = LpVariable("x_1", lowBound=0, cat="Integer")
-    x_2 = LpVariable("x_2", lowBound=0, cat="Integer")
-    x_3 = LpVariable("x_3", lowBound=0, cat="Integer")
-    x_4 = LpVariable("x_4", lowBound=0, cat="Integer")
-    x_5 = LpVariable("x_5", lowBound=0, cat="Integer")
-    x_6 = LpVariable("x_6", lowBound=0, cat="Integer")
-    x_7 = LpVariable("x_7", lowBound=0, cat="Integer")
-    x_8 = LpVariable("x_8", lowBound=0, cat="Integer")
+    variables = {i: LpVariable(f"x_{i}", lowBound=0, cat="Integer") for i in APPLIANCES.keys()}
 
     # Add the objective function to maximize
-
-    lp_problem += (coefficients[1] * x_1 + coefficients[2] * x_2 + coefficients[3] * x_3 + coefficients[4] * x_4 + coefficients[5] * x_5 + coefficients[6] * x_6 + coefficients[7] * x_7 + coefficients[8] * x_8)
+    lp_problem += (coefficients[1] * variables[1] + coefficients[2] * variables[2] + coefficients[3] * variables[3] + coefficients[4] * variables[4] + coefficients[5] * variables[5] + coefficients[6] * variables[6] + coefficients[7] * variables[7] + coefficients[8] * variables[8])
 
     # Add the constraints
-    lp_problem += (coefficients[1] * x_1 + coefficients[2] * x_2 + coefficients[3] * x_3 + coefficients[4] * x_4 + coefficients[5] * x_5 + coefficients[6] * x_6 + coefficients[7] * x_7 + coefficients[8] * x_8 <= max_energy_consumption)
-    if coefficients[1] == 0:
-        lp_problem += x_1 == 0
-    if coefficients[2] == 0:
-        lp_problem += x_2 == 0
-    if coefficients[3] == 0:
-        lp_problem += x_3 == 0
-    if coefficients[4] == 0:
-        lp_problem += x_4 == 0
-    if coefficients[5] == 0:
-        lp_problem += x_5 == 0
-    if coefficients[6] == 0:
-        lp_problem += x_6 == 0
-    if coefficients[7] == 0:
-        lp_problem += x_7 == 0
-    if coefficients[8] == 0:
-        lp_problem += x_8 == 0
+    lp_problem += (coefficients[1] * variables[1] + coefficients[2] * variables[2] + coefficients[3] * variables[3] + coefficients[4] * variables[4] + coefficients[5] * variables[5] + coefficients[6] * variables[6] + coefficients[7] * variables[7] + coefficients[8] * variables[8] <= max_energy_consumption)
+    for i in APPLIANCES.keys():
+        if coefficients[i] == 0:
+            lp_problem += variables[i] == 0
     
     if (coefficients[1] > 0 or coefficients[2] > 0):
-        lp_problem += (HOURS_CATEGORIES["F"]["min"] <= x_1 + x_2)
-        lp_problem += (x_1 + x_2 <= HOURS_CATEGORIES["F"]["max"])
+        lp_problem += (HOURS_CATEGORIES["F"]["min"] <= variables[1] + variables[2])
+        lp_problem += (variables[1] + variables[2] <= HOURS_CATEGORIES["F"]["max"])
     
     if (coefficients[3] > 0 or coefficients[4] > 0 or coefficients[5] > 0):
-        lp_problem += (HOURS_CATEGORIES["A"]["min"] <= x_3 + x_4 + x_5)
-        lp_problem += (x_3 + x_4 + x_5 <= HOURS_CATEGORIES["A"]["max"])
+        lp_problem += (HOURS_CATEGORIES["A"]["min"] <= variables[3] + variables[4] + variables[5])
+        lp_problem += (variables[3] + variables[4] + variables[5] <= HOURS_CATEGORIES["A"]["max"])
     
     if (coefficients[6] > 0 or coefficients[7] > 0 or coefficients[8] > 0):
-        lp_problem += (HOURS_CATEGORIES["L"]["min"] <= x_6 + x_7 + x_8)
-        lp_problem += (x_6 + x_7 + x_8 <= HOURS_CATEGORIES["L"]["max"])
+        lp_problem += (HOURS_CATEGORIES["L"]["min"] <= variables[6] + variables[7] + variables[8])
+        lp_problem += (variables[6] + variables[7] + variables[8] <= HOURS_CATEGORIES["L"]["max"])
     
     LpSolverDefault.msg = False
 
     # Solve the problem
     lp_problem.solve()
 
-    # Return the results
-    return {
-        1: round(x_1.value(),2) if x_1.value() is not None else 0,
-        2: round(x_2.value(),2) if x_2.value() is not None else 0,
-        3: round(x_3.value(),2) if x_3.value() is not None else 0,
-        4: round(x_4.value(),2) if x_4.value() is not None else 0,
-        5: round(x_5.value(),2) if x_5.value() is not None else 0,
-        6: round(x_6.value(),2) if x_6.value() is not None else 0,
-        7: round(x_7.value(),2) if x_7.value() is not None else 0,
-        8: round(x_8.value(),2) if x_8.value() is not None else 0,
-    }
+    return {i: round(variables[i].value(), 2) if variables[i].value() is not None else 0 for i in APPLIANCES.keys()}
