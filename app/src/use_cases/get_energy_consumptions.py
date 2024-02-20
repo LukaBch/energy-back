23505ac7748d662bd85139f264a6085a856cf484
LukaBch <lucas.banchereau@gmail.com>
2024-02-20 17:29:11 +0100

    refacto in get_energy_consumptions

--- app/src/use_cases/get_energy_consumptions.py
+++ app/src/use_cases/get_energy_consumptions.py
@@ -2,28 +2,32 @@ from .compute_hours_consumptions import compute_hour_consumptions
 from ..constants import APPLIANCES
 
 def get_energy_consumptions(selected_appliances, total_consumption):
-    hour_consumptions2 = compute_hour_consumptions(
+    hour_consumptions = compute_hour_consumptions(
         selected_appliances=selected_appliances,
         max_energy_consumption=total_consumption
     )
-    res = []
-    total_energy = 0
-    for appliance_id, appliance_info in APPLIANCES.items():
-        if appliance_id in selected_appliances:
-            power = appliance_info["power"]
-            energy = round(hour_consumptions2[appliance_id]*power/1000, 2)
-            total_energy += energy
-            res.append(
-              {"id": appliance_id, "hours": hour_consumptions2[appliance_id], "energy": energy}
-            )
-        else:
-            res.append(
-              {"id": appliance_id, "hours": 0, "energy": 0}
-            )
     
-    for result in res:
-        result["proportion"] = round(100*result["energy"]/total_energy, 2)
+    total_energy = sum(
+      [
+        round(hour_consumptions[appliance_id]*APPLIANCES[appliance_id]["power"]/1000, 2) 
+        for appliance_id in APPLIANCES.keys()
+      ]
+    )
+    
+    energy_consumptions = []
+    for appliance_id, appliance_info in APPLIANCES.items():
+        power = appliance_info["power"]
+        energy = round(hour_consumptions[appliance_id]*power/1000, 2)
+        energy_consumptions.append(
+          {
+            "id": appliance_id,
+            "hours": hour_consumptions[appliance_id],
+            "energy": energy,
+            "proportion": round(100*energy/total_energy, 2)
+          }
+        )
+    
     return {
-      "energies": res,
+      "energies": energy_consumptions,
       "total": round(total_energy, 2)
     }
